8b93314fa0bf539afb7cdf6f4c5d8f570b472a7a
A-MOHAMED14 <a.moh4med1@gmail.com>
2026-02-04 18:14:08 +0000

    Initialise project with TypeScript and CDK setup

---
+++ test/techhealth-cdk-migration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechhealthCdkMigration from '../lib/techhealth-cdk-migration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/techhealth-cdk-migration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechhealthCdkMigration.TechhealthCdkMigrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
