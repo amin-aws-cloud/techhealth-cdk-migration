274895a1943f1c359cfe7a3f89c89447a5bd822b
A-MOHAMED14 <a.moh4med1@gmail.com>
2026-02-14 15:12:12 +0000

    Add VPC with public and private subnets across 2 AZs

--- lib/techhealth-cdk-migration-stack.ts
+++ lib/techhealth-cdk-migration-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import necessary CDK libraries and constructs
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class TechhealthCdkMigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC with 2 AZs, 1 public and 1 private isolated subnet in each AZ
+    const vpc = new ec2.Vpc(this, "TechHealthVPC", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: "PublicSubnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "PrivateSubnet",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TechhealthCdkMigrationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Output the VPC ID in CloudFormation outputs for easy reference
+    new cdk.CfnOutput(this, "VPCId", {
+      value: vpc.vpcId,
+      description: "VPC ID",
+    });
   }
 }
